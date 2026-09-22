8ee4d73d190f17950026691a74766880f96eae35
MinJung Kim <alswnd0830@naver.com>
2026-09-22 10:23:15 +0900

    Initial commit

---
+++ src/main/java/com/example/classickey/ClassicKeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.classickey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassicKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/classickey/ClassicKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classickey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
